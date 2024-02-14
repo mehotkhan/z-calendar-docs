dfd33e46f0b3984c6316012b03587660b3e56dd1
Ali Zamani <ali.zama@mtnirancell.ir>
2024-02-14 15:00:43 +0330

    bootup Core Calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,25 +2,50 @@
 import { ref, computed, watch, onMounted } from 'vue'
 import { DateTime, Interval, Info } from "luxon";
 
-const locale = 'fa-IR'
-// const locale = 'en-GB'
-const inputDate = DateTime.local({ locale }).reconfigure({ outputCalendar: "persian" });
-// const inputDate = DateTime.local({ locale })
+const props = defineProps({
+    locale: String,
+    dir: String,
+    inputDate: Object,
+})
 
 
-const weekdaysShort = Info.weekdays('narrow', { locale })
 
 const monthSelector = ref(0)
+const dayOffset = ref(0)
 const days = ref([]);
 
 
+onMounted(() => {
+    const localParts = props.inputDate.toLocaleParts()
+    dayOffset.value = localParts[2].value
+})
 
-const currentDate = computed(() => inputDate.plus({ month: monthSelector.value }))
-const monthName = computed(() => currentDate.value.toFormat('LLLL'))
-const yearName = computed(() => currentDate.value.toFormat('yyyy'))
+watch(props, async (newProps) => {
+    const localParts = newProps.inputDate.toLocaleParts()
+    console.log(localParts)
+    console.log(newProps.inputDate)
 
-const start = computed(() => currentDate.value.startOf('month').startOf('week'))
-const end = computed(() => currentDate.value.endOf('month').endOf('week'))
+    const month = localParts[0].value
+    const day = localParts[2].value
+    dayOffset.value = day
+    console.log(month, day)
+})
+
+
+
+const weekdaysShort = computed(() => Info.weekdays('short', { locale: props.locale, useLocaleWeeks: true }))
+const currentDate = computed(() => props.inputDate.plus({ month: monthSelector.value }))
+const monthName = computed(() => currentDate.value.toFormat('LLLL', { locale: props.locale }))
+const yearName = computed(() => currentDate.value.toFormat('yyyy', { locale: props.locale }))
+// standard :) 
+// const start = computed(() => currentDate.value.startOf('month').startOf('week'))
+// const end = computed(() => currentDate.value.endOf('month').endOf('week'))
+
+
+// Hacked One :) 
+const start = computed(() => currentDate.value.minus({ day: dayOffset.value }).startOf('week'))
+const end = computed(() => start.value.plus({ day: 35 }).endOf('week'))
+
 
 const range = computed(() => Interval.fromDateTimes(start.value, end.value).splitBy({ days: 1 }))
 
@@ -28,16 +53,18 @@ const isToday = (date) => {
     return date.toISODate() === DateTime.local().toISODate();
 }
 const isMonth = (date) => {
-    return date.month === currentDate.value.month;
+    // console.log(date.toLocaleParts()[0].value)
+    return date.toLocaleParts()[0].value === currentDate.value.toLocaleParts()[0].value;
 }
 
 const weekGenerator = (rangeDays) => {
     days.value = []
     rangeDays.forEach((day) => {
         days.value.push({
-            dayNum: day.start.toFormat('dd'),
-            dayName: day.start.toFormat('cccc'),
-            dayClass: [isMonth(day.start) ? 'text-gray-500' : 'text-gray-300', isToday(day.start) ? 'bg-blue-400 text-gray-100 p-1' : '']
+            dayNum: day.start.toFormat('dd', { locale: props.locale }),
+            dayName: day.start.toFormat('cccc', { locale: props.locale }),
+            monthName: day.start.toFormat('MMM', { locale: props.locale }),
+            dayClass: [isMonth(day.start) ? 'text-gray-600' : 'text-gray-300', isToday(day.start) ? 'bg-blue-400 text-white p-1' : '']
         });
     })
 }
@@ -75,7 +102,7 @@ watch(range, async (newRange) => {
 
             </div>
         </div>
-        <div class="flex items-center justify-between pt-12 overflow-x-auto">
+        <div class="flex items-center justify-between pt-12  " :class="dir == 'rtl' && 'rtl'">
             <div class="w-full">
                 <div class="grid grid-cols-7 gap-0">
 
@@ -85,10 +112,14 @@ watch(range, async (newRange) => {
                 </div>
                 <div class="grid grid-cols-7 gap-0 mt-5 ">
 
-                    <div v-for="day in days" class="w-full flex justify-center">
-                        <div class="text-base font-medium rounded-full px-2 m-3" :class="day.dayClass">{{
+                    <div v-for="day in days" class="w-full flex justify-center flex-col px-2 m-3 items-center"
+                        :class="day.dayClass">
+
+
+                        <div class="text-2xl font-medium rounded-full">{{
                             day.dayNum
-                        }}</div>
+                        }}
+                        </div>
 
                     </div>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,45 @@
-<script setup>
+<script setup lang="ts">
+import { ref, computed, watch, onMounted } from 'vue'
+import { DateTime, Interval, Info } from "luxon";
 import Calendar from './Calendar.vue'
-import Events from './Events.vue'
+// import Events from './Events.vue'
+
+const lngMode = ref<string>('en')
+const dirMode = ref<string>('ltr')
+const locale = ref<string>('')
+const inputDate = ref<object>()
+
+const switchMode = () => {
+  lngMode.value == 'fa' ? lngMode.value = 'en' : lngMode.value = 'fa'
+  bootUp()
+}
+
+const bootUp = () => {
+  if (lngMode.value == 'fa') {
+    locale.value = 'fa-IR'
+    dirMode.value = 'rtl'
+    inputDate.value = DateTime.local().reconfigure({ outputCalendar: "persian" });
+  } else {
+    dirMode.value = 'ltr'
+    locale.value = 'en-GB'
+    inputDate.value = DateTime.local()
+  }
+}
+bootUp()
+
+
 
 
 </script>
 <template>
-  <div class="flex items-center justify-center py-8 px-4">
-
-    <div class="max-w-sm w-full shadow-lg">
-      <Calendar />
+  <div class="flex items-center justify-center py-8 px-4 flex-col gap-3">
+    <div>
+      <button @click="switchMode()"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded capitalize">
+        {{ lngMode }}</button>
+    </div>
+    <div class="max-w-xl w-full shadow-lg">
+      <Calendar :locale="locale" :inputDate="inputDate" :dir="dirMode" />
     </div>
   </div>
 </template>
